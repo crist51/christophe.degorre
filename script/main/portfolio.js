ea929f47d7a50db513fe7d9602821076c2244f87
crist51 <crist.degorre@gmail.com>
2023-02-09 06:17:45 +0100

    light update

--- script/main/portfolio.js
+++ script/main/portfolio.js
@@ -7,79 +7,78 @@ const portfolio = document.querySelector(".portfolio")
 if (portfolio != null) {
 
 
-    for (let b = projets.length; 0 < b; b--) {
-        const i = b-1
+  for (let b = projets.length; 0 < b; b--) {
+    const i = b - 1
 
-        let _card_mrvio_1 = document.createElement("div");
-        _card_mrvio_1.className= "_card_mrvio_1";
-        portfolio.appendChild(_card_mrvio_1);
+    let _card_mrvio_1 = document.createElement("div");
+    _card_mrvio_1.className = "_card_mrvio_1";
+    portfolio.appendChild(_card_mrvio_1);
 
-        let _titleCard_mrvio_25 = document.createElement("div");
-        _titleCard_mrvio_25.className= "_titleCard_mrvio_25";
-        _titleCard_mrvio_25.style=`background-image: url(${projets[i].imageURL});`;
-        _card_mrvio_1.appendChild(_titleCard_mrvio_25)
-        
-        let _title_mrvio_8 = document.createElement("h2");
-        _title_mrvio_8.className= "_title_mrvio_8";
-        _title_mrvio_8.textContent = projets[i].title;
-        _card_mrvio_1.appendChild(_titleCard_mrvio_25);
-        
-        let pa3 = document.createElement("div");
-        pa3.className= "pa3";
-        pa3.style= "padding:1.5rem"
-        _card_mrvio_1.appendChild(pa3)
+    let _titleCard_mrvio_25 = document.createElement("div");
+    _titleCard_mrvio_25.className = "_titleCard_mrvio_25";
+    _titleCard_mrvio_25.style = `background-image: url(${projets[i].imageURL});`;
+    _card_mrvio_1.appendChild(_titleCard_mrvio_25)
 
-        // ---------- insert tag Array boucle
+    let _title_mrvio_8 = document.createElement("h2");
+    _title_mrvio_8.className = "_title_mrvio_8";
+    _title_mrvio_8.textContent = projets[i].title;
+    _card_mrvio_1.appendChild(_titleCard_mrvio_25);
 
-        let _tags_mrvio_76 = document.createElement("div");
-        pa3.appendChild(_tags_mrvio_76);
-        _tags_mrvio_76.textContent = "Tag :"
+    let pa3 = document.createElement("div");
+    pa3.className = "pa3";
+    pa3.style = "padding:1.5rem"
+    _card_mrvio_1.appendChild(pa3)
 
-        for (let index = 0; index < projets[i].tag.length; index++) {
+    // ---------- insert tag Array boucle
 
-            let div = document.createElement("div");
-            div.textContent = `${projets[i].tag[index]}`
-            div.className = "_tag_mrvio_76"
-            _tags_mrvio_76.appendChild(div);
-            
-        }
+    let _tags_mrvio_76 = document.createElement("div");
+    pa3.appendChild(_tags_mrvio_76);
+    _tags_mrvio_76.textContent = "Tag :"
 
-        let _desc_mrvio_32 = document.createElement("p");
-        _desc_mrvio_32.className= "_desc_mrvio_32 mt0 mb2";
-        _desc_mrvio_32.textContent = projets[i].desicription;
-        pa3.appendChild(_desc_mrvio_32);
+    for (let index = 0; index < projets[i].tag.length; index++) {
 
-        let _link_mrvio_37 = document.createElement("a");
-        _link_mrvio_37.href = "./project.html?_id=" + projets[i]._id;
-        _link_mrvio_37.title = `"lien vers project: ${projets[i].title}"`;
-        _link_mrvio_37.className= "_link_mrvio_37";
-        pa3.appendChild(_link_mrvio_37);
-
-
-        let _linkInner_mrvio_59 = document.createElement("span");
-        _linkInner_mrvio_59.className= "_linkInner_mrvio_59";
-        _linkInner_mrvio_59.textContent = projets[i].title
-        _link_mrvio_37.appendChild(_linkInner_mrvio_59);
+      let div = document.createElement("div");
+      div.textContent = `${projets[i].tag[index]}`
+      div.className = "_tag_mrvio_76"
+      _tags_mrvio_76.appendChild(div);
 
     }
 
-}
+    let _desc_mrvio_32 = document.createElement("p");
+    _desc_mrvio_32.className = "_desc_mrvio_32 mt0 mb2";
+    _desc_mrvio_32.textContent = projets[i].desicription;
+    pa3.appendChild(_desc_mrvio_32);
 
-// ---------------  Scroll animations --------------- \\
+    let _link_mrvio_37 = document.createElement("a");
+    _link_mrvio_37.href = "./project.html?_id=" + projets[i]._id;
+    _link_mrvio_37.title = `"lien vers project: ${projets[i].title}"`;
+    _link_mrvio_37.className = "_link_mrvio_37";
+    pa3.appendChild(_link_mrvio_37);
 
-const generalAnimatedElements = [
+
+    let _linkInner_mrvio_59 = document.createElement("span");
+    _linkInner_mrvio_59.className = "_linkInner_mrvio_59";
+    _linkInner_mrvio_59.textContent = projets[i].title
+    _link_mrvio_37.appendChild(_linkInner_mrvio_59);
+
+  }
+
+
+  // ---------------  Scroll animations --------------- \\
+
+  const generalAnimatedElements = [
     ...document.querySelectorAll("._card_mrvio_1"),
   ];
-  
+
   const animatedContents = [
     ...generalAnimatedElements,
   ]
-  
+
   const intersectionObserver = new IntersectionObserver(handleIntersect, { rootMargin: "-20%" })
   // we listen to the event at 20% off
-  
+
   animatedContents.forEach(el => intersectionObserver.observe(el))
-  
+
   function handleIntersect(entries) {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
@@ -89,3 +88,4 @@ const generalAnimatedElements = [
       }
     })
   }
+}
